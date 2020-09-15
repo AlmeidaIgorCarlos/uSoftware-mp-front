df28d5592a708f2f88d93bc584825e84c2480dc7
AlmeidaIgorCarlos <almeidaigorcarlos@gmail.com>
2020-09-14 22:01:55 -0300

    preview design of configuration and reports view

--- src/views/configuration.vue
+++ src/views/configuration.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>configuration</div>
+  <v-row justify="center">
+    <v-row justify="center">
+      <v-col cols="12" sm="4" >
+        <v-card class="left-padding right-padding top-padding" style="padding-bottom: 20px">
+          <v-text-field label="Id" placeholder="Id" readonly></v-text-field>
+          <v-text-field label="Nome" placeholder="Nome"></v-text-field>
+          <v-text-field label="Login" placeholder="Login"></v-text-field>
+          <v-text-field label="Senha" placeholder="Senha"></v-text-field>
+          <v-text-field
+            label="Confirmação de Senha"
+            placeholder="Confirmação de Senha"
+            type="passoword"
+          ></v-text-field>
+          <v-row justify="center">
+            <v-btn color="primary">Salvar</v-btn>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-row>
 </template>
 
 <script>
@@ -7,4 +26,23 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.top-padding {
+  padding-top: 3%;
+}
+
+.left-padding {
+  padding-left: 3%;
+}
+
+.right-padding {
+  padding-right: 3%;
+}
+
+.card-height {
+  height: 80vh;
+}
+
+.card-scroll {
+  overflow-y: scroll;
+}
 </style>
--- src/views/reports.vue
+++ src/views/reports.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>reports</div>
+    <v-row justify="start">
+      <v-col cols="12" sm="12" >
+        <v-card class="left-padding right-padding">
+          <v-select label="Selecione seu Relatório" :items="units"></v-select>
+        </v-card>
+      </v-col>
+    </v-row>
 </template>
 
 <script>
@@ -7,4 +13,26 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.top-padding {
+  padding-top: 3%;
+}
+
+.left-padding {
+  padding-left: 3%;
+}
+
+.right-padding {
+  padding-right: 3%;
+}
+
+.card-height {
+  height: 80vh;
+}
+
+.card-scroll {
+  overflow-y: scroll;
+}
+
+.item-order {
+}
 </style>
