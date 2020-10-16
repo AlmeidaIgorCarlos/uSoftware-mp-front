da41d85e3cc1a56765eee458c3eaf9c5775cce74
AlmeidaIgorCarlos <almeidaigorcarlos@gmail.com>
2020-10-15 21:21:46 -0300

    Finishing reports and improving old-reports

--- src/views/old-orders.vue
+++ src/views/old-orders.vue
@@ -1,22 +1,23 @@
 <template>
   <v-row justify="start">
     <v-col cols="12" md="12" lg="8">
-      <v-card>
-        <v-row class="left-padding top-padding" justify="start">
+      <v-card >
+        <v-row v-if="selectedAttendance" class="left-padding top-padding" justify="start">
           <v-col cols="12" sm="6">
-            <v-text-field placeholder="Protocolo" label="Protocolo de Atendimento" readonly />
+            <v-text-field placeholder="Protocolo" label="Protocolo de Atendimento" :value="selectedAttendance.protocol" readonly />
           </v-col>
-          <v-col cols="12" sm="6">
-            <v-select label="Status do Atendimento" :items="units"></v-select>
-          </v-col>
-          <v-col cols="12" sm="12">
+          <v-col cols="11" sm="11">
             <v-textarea
               outlined
               name="input-7-4"
               label="Tratativa"
-              value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+              v-model="newComment"
+              readonly
             ></v-textarea>
-              <v-file-input accept="image/*" label="Upload de documento"></v-file-input>
+            <v-row justify="space-around">
+            <v-btn color="primary">Salvar</v-btn>
+            <v-btn color="error">Encerrar Atendimento</v-btn>
+            </v-row>
           </v-col>
         </v-row>
       </v-card>
@@ -26,17 +27,17 @@
         <v-row justify="center">
           <v-label>Atendimentos Solicitados</v-label>
         </v-row>
-        <v-form class="item-order">
+        <v-form class="item-order" v-for="attendance in attendances" :key="attendance.id">
           <v-row justify="start">
             <v-col cols="12" sm="6">
-              <v-text-field placeholder="Protocolo" label="Protocolo de Atendimento" readonly />
-              <v-text-field placeholder="Data de Solicitação" label="Data de Solicitação" readonly />
-              <v-text-field placeholder="Nome do cidadão" label="Nome do cidadão" readonly />
+              <v-text-field placeholder="Protocolo" label="Protocolo de Atendimento" :value="attendance.protocol" readonly />
+              <v-text-field placeholder="Data de Solicitação" label="Data de Solicitação" :value="new Date(attendance.attendanceDate).toLocaleString('pt-br').substring(0, 10)" readonly />
+              <v-text-field placeholder="Nome do cidadão" label="Nome do cidadão" :value="attendance.userName" readonly />
             </v-col>
             <v-col cols="12" sm="6">
-              <v-text-field placeholder="RG" label="RG" />
-              <v-text-field placeholder="CPF" label="CPF" />
-              <v-text-field placeholder="Telefone" label="Telefone" />
+              <v-text-field placeholder="RG" label="RG" :value="attendance.userRG" readonly/>
+              <v-text-field placeholder="CPF" label="CPF" :value="attendance.userCPF" readonly/>
+              <v-text-field placeholder="Telefone" label="Telefone" :value="attendance.userPhoneNumber" readonly/>
             </v-col>
             <v-row justify="end">
               <v-col cols="12" sm="12">
@@ -44,59 +45,10 @@
                   outlined
                   name="input-7-4"
                   label="Descrição"
-                  value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+                  :value="attendance.description"
+                  readonly
                 ></v-textarea>
-                <v-btn color="primary">Selecionar</v-btn>
-              </v-col>
-            </v-row>
-          </v-row>
-        </v-form>
-        <v-form class="item-order">
-          <v-row justify="start">
-            <v-col cols="12" sm="6">
-              <v-text-field placeholder="Protocolo" label="Protocolo de Atendimento" readonly />
-              <v-text-field placeholder="Data de Solicitação" label="Data de Solicitação" readonly />
-              <v-text-field placeholder="Nome do cidadão" label="Nome do cidadão" readonly />
-            </v-col>
-            <v-col cols="12" sm="6">
-              <v-text-field placeholder="RG" label="RG" />
-              <v-text-field placeholder="CPF" label="CPF" />
-              <v-text-field placeholder="Telefone" label="Telefone" />
-            </v-col>
-            <v-row justify="end">
-              <v-col cols="12" sm="12">
-                <v-textarea
-                  outlined
-                  name="input-7-4"
-                  label="Descrição"
-                  value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-                ></v-textarea>
-                <v-btn color="primary">Selecionar</v-btn>
-              </v-col>
-            </v-row>
-          </v-row>
-        </v-form>
-        <v-form class="item-order">
-          <v-row justify="start">
-            <v-col cols="12" sm="6">
-              <v-text-field placeholder="Protocolo" label="Protocolo de Atendimento" readonly />
-              <v-text-field placeholder="Data de Solicitação" label="Data de Solicitação" readonly />
-              <v-text-field placeholder="Nome do cidadão" label="Nome do cidadão" readonly />
-            </v-col>
-            <v-col cols="12" sm="6">
-              <v-text-field placeholder="RG" label="RG" />
-              <v-text-field placeholder="CPF" label="CPF" />
-              <v-text-field placeholder="Telefone" label="Telefone" />
-            </v-col>
-            <v-row justify="end">
-              <v-col cols="12" sm="12">
-                <v-textarea
-                  outlined
-                  name="input-7-4"
-                  label="Descrição"
-                  value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-                ></v-textarea>
-                <v-btn color="primary">Selecionar</v-btn>
+                <v-btn color="primary" @click="selectAttendance(attendance)">Selecionar</v-btn>
               </v-col>
             </v-row>
           </v-row>
@@ -107,7 +59,41 @@
 </template>
 
 <script>
-export default {};
+import AttendanceService from "../services/attendance.service";
+import { Session } from "../services/session.enum";
+
+export default {
+  async created() {
+    const { accessToken, id } = JSON.parse(
+      sessionStorage.getItem(Session.usoftwareUser)
+    );
+    this.accessToken = accessToken;
+    this.id = id;
+
+    const attendanceService = new AttendanceService();
+    this.attendanceService = attendanceService;
+
+    const attendances = await attendanceService.getAll(accessToken);
+    this.attendances = attendances.filter((item) => item.prosecutorId === id);
+  },
+  data: function () {
+    return {
+      attendances: [],
+      accessToken: "",
+      id: "",
+      attendanceService: new AttendanceService(),
+      selectedAttendance: undefined,
+      newComment: ""
+    };
+  },
+  methods:{
+    selectAttendance(attendance){
+      this.newComment = ""
+      this.selectedAttendance = attendance
+    }
+  }
+}
+
 </script>
 
 <style lang="scss" scoped>
--- src/views/reports.vue
+++ src/views/reports.vue
@@ -1,38 +1,115 @@
 <template>
-    <v-row justify="start">
-      <v-col cols="12" sm="12" >
-        <v-card class="left-padding right-padding">
-          <v-select label="Selecione seu Relatório" :items="units"></v-select>
+<v-row justify="center">
+    <div class="small">
+        <v-card>
+            <LineChart :chart-data="datacollection"></LineChart>
         </v-card>
-      </v-col>
-    </v-row>
+    </div>
+</v-row>
 </template>
 
 <script>
-export default {};
+import LineChart from "./../components/line-chart.js";
+import AttendanceService from "../services/attendance.service";
+import {
+    Session
+} from "../services/session.enum";
+
+export default {
+    async created() {
+        const {
+            accessToken,
+            id
+        } = JSON.parse(
+            sessionStorage.getItem(Session.usoftwareUser)
+        );
+        this.accessToken = accessToken;
+        this.id = id;
+
+        const attendanceService = new AttendanceService();
+        this.attendanceService = attendanceService;
+
+        this.attendances = await attendanceService.getAll(accessToken);
+
+        this.attendances.forEach(item => {
+            const date = new Date(item.attendanceDate)
+            this.months.push(date.getMonth())
+        })
+    },
+    components: {
+        LineChart,
+    },
+    data() {
+        return {
+            datacollection: null,
+            accessToken: '',
+            id: '',
+            months: []
+        };
+    },
+    mounted() {
+        this.fillData();
+    },
+    watch: {
+        months: function () {
+            this.fillData();
+        }
+    },
+    methods: {
+        fillData() {
+            this.datacollection = {
+                labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+                datasets: [{
+                    label: "Atendimentos",
+                    backgroundColor: "rgb(5, 63, 94)",
+                    fill: false,
+                    data: [
+                        this.months.filter(i => i === 0).length,
+                        this.months.filter(i => i === 1).length,
+                        this.months.filter(i => i === 2).length,
+                        this.months.filter(i => i === 3).length,
+                        this.months.filter(i => i === 4).length,
+                        this.months.filter(i => i === 5).length,
+                        this.months.filter(i => i === 6).length,
+                        this.months.filter(i => i === 7).length,
+                        this.months.filter(i => i === 8).length,
+                        this.months.filter(i => i === 9).length,
+                        this.months.filter(i => i === 10).length,
+                        this.months.filter(i => i === 11).length,
+                    ]
+                }],
+            };
+        }
+    },
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .top-padding {
-  padding-top: 3%;
+    padding-top: 3%;
 }
 
 .left-padding {
-  padding-left: 3%;
+    padding-left: 3%;
 }
 
 .right-padding {
-  padding-right: 3%;
+    padding-right: 3%;
 }
 
 .card-height {
-  height: 80vh;
+    height: 80vh;
 }
 
 .card-scroll {
-  overflow-y: scroll;
+    overflow-y: scroll;
 }
 
-.item-order {
+.small {
+    min-width: 40%;
+}
+
+canvas {
+    max-height: 85vh !important;
 }
 </style>
